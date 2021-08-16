377f2462cea915842fa3d1ac2fcd9237cb8283e3
andythebreaker <e24074724@mail.ncku.edu.tw>
2021-08-16 22:41:11 +0800

    to switch 2 api

--- src/components/modals/Award.vue
+++ src/components/modals/Award.vue
@@ -6,6 +6,21 @@
     @on-visible-change="visibleChange"
   >
     <h1>{{ IsDone_data }}</h1>
+    <div style="max-width: 100vh;">
+      <div
+        style="left: 0; width: 100%; height: 0; position: relative; padding-bottom: 100%;"
+      >
+        <iframe
+          id="streamablevideo"
+          :src="video_url"
+          style="top: 0; left: 0; width: 100%; height: 100%; position: absolute; border: 0;"
+          allowfullscreen
+          scrolling="no"
+          allow="encrypted-media;"
+          @load="onMyFrameLoad"
+        ></iframe>
+      </div>
+    </div>
     <div slot="footer">
       <Button type="primary" size="large" @click="closeModal"
         >{{ $t("common.ok") }}
@@ -24,18 +39,21 @@ export default {
   },
   methods: {
     visibleChange(isVisible) {
-      
       if (!isVisible) {
         this.closeModal();
       }
     },
     closeModal() {
       this.$store.dispatch("hideAward");
+    },
+    onMyFrameLoad(event) {
+      this.IsDone_data=event.target.id;
     }
   },
   data() {
     return {
-      IsDone_data: "wtf, it shouldn't be like this!"
+      IsDone_data: "wtf, it shouldn't be like this!",
+      video_url: `https://streamable.com/o/1kb8dl`
     };
   }
 };
